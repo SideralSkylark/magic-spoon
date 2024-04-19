e1a954cda0ea7cecb27cb049ccd91d6e545240d5
SideralSkylark <152505167+SideralSkylark@users.noreply.github.com>
2024-04-19 10:46:54 +0200

    adicionado a imagem de fundo da paguina index.html

---
+++ php/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="header-links-left" class="header-links-left homePage">
+                <li><a href="about.html">ABOUT US</a></li>
+            </ul>
+            <div class="header-logo">
+                <a href="index.html">
+                    <span class="magic">MAGIC</span>
+                    <span class="spoon">SPOON</span>
+                </a>
+            </div>
+            <ul class="header-links-right">
+                <li>
+                    <a href="shopnow.html" class="shopnow">SHOP NOW</a>
+                    <ul>
+                        <li><a href="cereal.html">CEREAL</a></li>
+                        <li><a href="treats.html">TREATS</a></li>
+                        <li><a href="bundles.html">BUNDLES</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="header-actions">
+                <a href="account.html"><img src="/Assets/icons/user.png" alt="user"></a>
+                <a href="cart.html"><img src="/Assets/icons/magic-hat.png" alt="cart"></a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
