df4937c262901403498de7e7e1a36b37159a2d2b
SideralSkylark <152505167+SideralSkylark@users.noreply.github.com>
2024-04-26 12:16:16 +0200

    correcao de erros

---
+++ app/core/Request.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\core;
+
+class Request {
+    public function getPath() {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $posotion = strpos($path,'?');
+
+        if ($posotion === false) {
+            return $path;
+        }
+
+        return substr($path,0, $posotion);
+    }
+
+    public function getMethod() {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+}
